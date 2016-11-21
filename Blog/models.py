634c320c26d595aa630b465b0df89900963070ee
Max Ungless <maxungless@gmail.com>
2016-11-21 21:07:13 +0000

    Added LiveCoding embed and posts

--- Blog/models.py
+++ Blog/models.py
@@ -1,11 +1,15 @@
 from django.db import models
-
+from django.template.defaultfilters import slugify
 
 class Post(models.Model):
     title = models.CharField(max_length=120)
     content = models.TextField()
     id = models.AutoField(primary_key=True)
-
+    slug = models.SlugField()
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Post, self).save(*args, **kwargs)
--- Blog/views.py
+++ Blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 
 def index(request):
-    return render(request, "Blog/index.html")
+    posts = Post.objects.all()
+    return render(request, "Blog/index.html", {"posts": posts})
